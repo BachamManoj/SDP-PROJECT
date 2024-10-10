6ff126c5d6d662e4b7665132e0e1d11eae65e273
BACHAM MANOJ KUMAR REDDY <2200030709@kluniversity.in>
2024-10-10 21:42:30 +0530

    saved successfully

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './pages/Navbar';
+import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './pages/Footer';
+import About from './pages/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import Service from './pages/Service';
+import Contact from './pages/Contact';
+import LoginPage from './pages/Login';
+import DoctorsBySpecialty from './pages/Doctor';
+import Appointment from './pages/Appointment';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/service' element={<Service/>} />
+          <Route exact path='/contact' element={<Contact/>} />
+          <Route exact path='/login' element={<LoginPage/>} />
+          <Route exact path='/doctor' element={<DoctorsBySpecialty/>} />
+          <Route exact path='/appointment' element={<Appointment/>} />
+        </Routes>
+        <Appointment/>
+        <Footer/>
+      </Router>
     </div>
   );
 }
